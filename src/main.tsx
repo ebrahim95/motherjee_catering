f963c3c4baed5815596ed8b9713568002ec457db
Ebrahim Haji <ebrahim.akh95@gmail.com>
2023-06-28 00:34:06 -0500

    added a new router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+// import App from './App.tsx'
+import { Home } from "./components/Home.tsx"
+
+import { FoodType } from "./components/FoodType.tsx"
+
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "food_category/:type",
+    element: <FoodType />
+  }
+]);
+
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )

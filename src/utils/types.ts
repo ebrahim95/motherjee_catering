b821797bd5ebdc00a01a32dc68e189d2df3c64be
Ebrahim Haji <ebrahim.akh95@gmail.com>
2023-07-01 00:29:42 -0500

    new type created

---
+++ src/utils/types.ts
@@ -0,0 +1,12 @@
+export interface results_types {
+  Category: string,
+  Name: string,
+  Description: string,
+  Full_tray?: string,
+  Half_tray?: string,
+  Image: string,
+  Bengali: string
+  Amount?: string
+}
+
+

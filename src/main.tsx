acdec3c1f632d9f6667b7ad9482dafc185de2548
Ebrahim Haji <ebrahim.akh95@gmail.com>
2023-07-01 00:30:05 -0500

    router moved to App

--- src/main.tsx
+++ src/main.tsx
@@ -1,31 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import App from './App.tsx'
-import { Home } from "./components/Home.tsx"
-
-import { FoodType } from "./components/FoodType.tsx"
-
+import App from './App.tsx'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "food_category/:type",
-    element: <FoodType />
-  }
-]);
-
-
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )

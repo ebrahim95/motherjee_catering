02b680b075569e67416c7e4002539f8f23330519
Ebrahim Haji <ebrahim.akh95@gmail.com>
2023-07-14 23:38:38 -0500

    removed grid rows and added justify-between

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -6,11 +6,11 @@ interface Props {
 export function Home(props: Props) {
 
   return (
-    <div className="mx-auto w-5/6 mt-12 flex flex-col justify-between items-center" >
+    <div className="mx-auto w-5/6 mt-12 flex flex-col items-center" >
       <h1>
         MotherJee's Catering
       </h1>
-      <div className="grid grid-cols-2 lg:grid-cols-3 lg:grid-rows-4 gap-6">
+      <div className="grid grid-cols-2 lg:grid-cols-3 gap-6">
         {props.csv.map((value, index) => <Cardfood key={index} heading={value} />)}
       </div>
     </div >

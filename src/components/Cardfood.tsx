b33b974527dd21b843ea62ad16c8f3c2013e7421
Ebrahim Haji <ebrahim.akh95@gmail.com>
2023-07-14 23:48:41 -0500

    changed styling

--- src/components/Cardfood.tsx
+++ src/components/Cardfood.tsx
@@ -8,11 +8,11 @@ interface Props {
 export const Cardfood = (props: Props) => {
 
   return (
-    <div className="card card-compact max-w-sm bg-base-100 ">
-      <figure className='m-0'>
+    <div className="card card-compact max-w-sm bg-base-100  shadow-sm shadow-black ">
+      <figure className='ring-1 ring-black box-sh m-0'>
         <img src="indian-sweet.webp" alt="indian-sweet" />
       </figure>
-      <div className="card-body items-center ring-1 ring-accent rounded-b-xl  hover:bg-accent-focus hover:rounded-b-xl">
+      <div className="card-body items-center rounded-b-lg ring-1 ring-black hover:bg-accent-focus hover:ring-2 ">
         <Link className="card-title m-0 font-bold no-underline" to={`food_category/${props.heading}`}>{props.heading}</Link>
       </div>
     </div >

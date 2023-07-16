86192875c8391c93a1d45ec8dd14700a841eeaf2
Ebrahim Haji <ebrahim.akh95@gmail.com>
2023-07-16 00:28:42 -0500

    edited css

--- src/components/Cardfood.tsx
+++ src/components/Cardfood.tsx
@@ -9,10 +9,10 @@ export const Cardfood = (props: Props) => {
 
   return (
     <div className="card card-compact max-w-sm bg-base-100  shadow-sm shadow-black ">
-      <figure className='ring-1 ring-black box-sh m-0'>
+      <figure className='ring-2 ring-accent  m-0'>
         <img src="indian-sweet.webp" alt="indian-sweet" />
       </figure>
-      <div className="card-body items-center rounded-b-lg ring-1 ring-black hover:bg-accent-focus hover:ring-2 ">
+      <div className="card-body bg-secondary items-center rounded-b-lg ring-2 ring-accent hover:bg-accent-focus hover:ring ">
         <Link className="card-title m-0 font-bold no-underline" to={`food_category/${props.heading}`}>{props.heading}</Link>
       </div>
     </div >

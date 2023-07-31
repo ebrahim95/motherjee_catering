a0ea6481b8aab4f4924b911a7c52daedb0e2d7e1
Ebrahim Haji <ebrahim.akh95@gmail.com>
2023-07-30 21:26:45 -0500

    added new styles and added prop.heading to alt

--- src/components/Cardfood.tsx
+++ src/components/Cardfood.tsx
@@ -2,20 +2,22 @@ import { Link } from 'react-router-dom'
 
 
 interface Props {
-  heading: string
+    heading: string,
+    image: string,
 }
+
 
 export const Cardfood = (props: Props) => {
 
-  return (
-    <div className="card card-compact max-w-sm bg-base-100  shadow-sm shadow-black ">
-      <figure className='ring-2 ring-accent  m-0'>
-        <img src="indian-sweet.webp" alt="indian-sweet" />
-      </figure>
-      <div className="card-body bg-secondary items-center rounded-b-lg ring-2 ring-accent hover:bg-accent-focus hover:ring ">
-        <Link className="card-title m-0 font-bold no-underline" to={`food_category/${props.heading}`}>{props.heading}</Link>
-      </div>
-    </div >
-  )
+    return (
+        <div className="card card-compact max-w-sm bg-base-100 ring-2 ring-amber-700  outline outline-2 outline-amber-400 outline-offset-4 rounded-2xl">
+            <figure className='ring-2 ring-accent m-0'>
+                <img className="w-72 h-48 lg:h-56 " src={props.image} alt={props.heading} />
+            </figure>
+            <div className="card-body bg-secondary items-center ring-2 ring-amber-900 rounded-b-2xl hover:bg-accent-focus hover:ring ">
+                <Link className="card-title m-0 font-bold no-underline" to={`food_category/${props.heading}`}>{props.heading}</Link>
+            </div>
+        </div >
+    )
 }
 

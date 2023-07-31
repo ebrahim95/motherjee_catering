856fb57029e621da38a5b3a85c5c4f4137603fea
Ebrahim Haji <ebrahim.akh95@gmail.com>
2023-07-30 21:29:26 -0500

    updated props

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,11 +1,48 @@
 import { Cardfood } from "./Cardfood"
 import { Link } from "react-router-dom"
-interface Props {
-    csv: string[]
-}
 
-function Home(props: Props) {
 
+
+
+function Home() {
+    const image_src = [
+        {
+            "heading": "Appetizers",
+            "image": "appetizers.webp"
+        }, {
+            "heading": "Beef and Curries",
+            "image": "beef_curry.webp"
+        },
+        {
+            "heading": "Biryanis and Rice",
+            "image": "biryani_and_rices.webp"
+        },
+        {
+            "heading": "Chicken and Curries",
+            "image": "chicken_curry.webp"
+        },
+        {
+            "heading": "Fish and Curries",
+            "image": "fish_curry.webp"
+        },
+        {
+            "heading": "Goat and Curries",
+            "image": "goat_curry.webp"
+        },
+        {
+            "heading": "Mishti and Pitha",
+            "image": "mishti.webp"
+        },
+        {
+            "heading": "Tandoori and Kabab",
+            "image": "kebab.webp"
+        },
+        {
+            "heading": "Bangladesi Chinese",
+            "image": "bangladesi_chinese.webp"
+        },
+
+    ]
     return (
         <div className="mx-auto w-5/6 mt-12 flex flex-col items-center" >
             <div className="text-lg breadcrumbs">
@@ -16,8 +53,8 @@ function Home(props: Props) {
             <h1>
                 MotherJee's Catering
             </h1>
-            <div className="grid grid-cols-2 lg:grid-cols-3 gap-6">
-                {props.csv.map((value, index) => <Cardfood key={index} heading={value} />)}
+            <div className="grid grid-cols-1 md:grid-cols-2 md:gap-4 lg:grid-cols-3 lg:gap-6">
+                {image_src.map((obj, index) => <Cardfood key={index} heading={obj.heading} image={obj.image} />)}
             </div>
         </div >
     )

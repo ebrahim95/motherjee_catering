c7217915160ef57e763599c9fc7007fed3295c3e
Ebrahim Haji <ebrahim.akh95@gmail.com>
2023-08-01 21:24:06 -0500

    changed width

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,59 +5,59 @@ import { Link } from "react-router-dom"
 
 
 function Home() {
-    const image_src = [
-        {
-            "heading": "Appetizers",
-            "image": "appetizers.webp"
-        }, {
-            "heading": "Beef and Curries",
-            "image": "beef_curry.webp"
-        },
-        {
-            "heading": "Biryanis and Rice",
-            "image": "biryani_and_rices.webp"
-        },
-        {
-            "heading": "Chicken and Curries",
-            "image": "chicken_curry.webp"
-        },
-        {
-            "heading": "Fish and Curries",
-            "image": "fish_curry.webp"
-        },
-        {
-            "heading": "Goat and Curries",
-            "image": "goat_curry.webp"
-        },
-        {
-            "heading": "Mishti and Pitha",
-            "image": "mishti.webp"
-        },
-        {
-            "heading": "Tandoori and Kabab",
-            "image": "kebab.webp"
-        },
-        {
-            "heading": "Bangladesi Chinese",
-            "image": "bangladesi_chinese.webp"
-        },
+  const image_src = [
+    {
+      "heading": "Appetizers",
+      "image": "appetizers.webp"
+    }, {
+      "heading": "Beef and Curries",
+      "image": "beef_curry.webp"
+    },
+    {
+      "heading": "Biryanis and Rice",
+      "image": "biryani_and_rices.webp"
+    },
+    {
+      "heading": "Chicken and Curries",
+      "image": "chicken_curry.webp"
+    },
+    {
+      "heading": "Fish and Curries",
+      "image": "fish_curry.webp"
+    },
+    {
+      "heading": "Goat and Curries",
+      "image": "goat_curry.webp"
+    },
+    {
+      "heading": "Mishti and Pitha",
+      "image": "mishti.webp"
+    },
+    {
+      "heading": "Tandoori and Kabab",
+      "image": "kebab.webp"
+    },
+    {
+      "heading": "Bangladesi Chinese",
+      "image": "bangladesi_chinese.webp"
+    },
 
-    ]
-    return (
-        <div className="mx-auto w-5/6 mt-12 flex flex-col items-center" >
-            <div className="text-lg breadcrumbs">
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                </ul>
-            </div>
-            <h1>
-                MotherJee's Catering
-            </h1>
-            <div className="grid grid-cols-1 md:grid-cols-2 md:gap-4 lg:grid-cols-3 lg:gap-6">
-                {image_src.map((obj, index) => <Cardfood key={index} heading={obj.heading} image={obj.image} />)}
-            </div>
-        </div >
-    )
+  ]
+  return (
+    <div className="mx-auto lg:w-5/6 w-11/12 mt-12 flex flex-col items-center" >
+      <div className="text-lg breadcrumbs">
+        <ul>
+          <li><Link to="/">Home</Link></li>
+        </ul>
+      </div>
+      <h1>
+        MotherJee's Catering
+      </h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 md:gap-4 lg:grid-cols-3 lg:gap-6">
+        {image_src.map((obj, index) => <Cardfood key={index} heading={obj.heading} image={obj.image} />)}
+      </div>
+    </div >
+  )
 }
 
 export default Home

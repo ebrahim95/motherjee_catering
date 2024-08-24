1a39d408b9c3fc279e2c6de8cd4eaec479ba911b
Ebrahim Haji <ebrahim.akh95@gmail.com>
2024-08-24 16:14:32 -0500

    style: seperated breadcrumbs and added a bg color to it

--- src/components/FoodType.tsx
+++ src/components/FoodType.tsx
@@ -31,30 +31,30 @@ const FoodCard = (props: result_no_catergoy) => {
 export function FoodType(props: Props) {
   const { type } = useParams();
   return (
-
-    <div className="mx-auto w-11/12 lg:w-5/6 mt-12 flex flex-col items-center" >
-      <div className="text-lg breadcrumbs">
-        <ul>
-          <li><Link to="/">Home</Link></li>
+    <>
+      <div className="text-lg breadcrumbs bg-red-900 w-full place-items-center">
+        <ul className="place-content-center h-4 text-white">
+          <li><Link to="/" className="text-white">Home</Link></li>
           <li>{type}</li>
         </ul>
       </div>
-
-      <h1>{type}</h1>
-      <div className="grid grid-cols-2 gap-8 md:grid-cols-2 md:gap-x-20 lg:grid-cols-3 lg:gap-8 ">
-        {props.csv.map((item_object, i) => {
-          if (item_object.Category === type) {
-            return <FoodCard key={i}
-              Name={item_object.Name}
-              Description={item_object.Description}
-              Image={item_object.Image}
-              Half_tray={item_object.Half_tray}
-              Full_tray={item_object.Full_tray}
-              Bengali={item_object.Bengali}
-            />
-          }
-        })}
+      <div className="mx-auto w-11/12 lg:w-5/6 mt-12 flex flex-col items-center" >
+        <h1>{type}</h1>
+        <div className="grid grid-cols-2 gap-8 md:grid-cols-2 md:gap-x-20 lg:grid-cols-3 lg:gap-8 ">
+          {props.csv.map((item_object, i) => {
+            if (item_object.Category === type) {
+              return <FoodCard key={i}
+                Name={item_object.Name}
+                Description={item_object.Description}
+                Image={item_object.Image}
+                Half_tray={item_object.Half_tray}
+                Full_tray={item_object.Full_tray}
+                Bengali={item_object.Bengali}
+              />
+            }
+          })}
+        </div>
       </div>
-    </div>
+    </>
   )
 }

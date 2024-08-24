71ed537bc606b4902284bec16051224a6990a368
Ebrahim Haji <ebrahim.akh95@gmail.com>
2024-08-24 16:11:59 -0500

    chore: changed image to link and uppercased headings

--- src/components/Cardfood.tsx
+++ src/components/Cardfood.tsx
@@ -12,10 +12,12 @@ export const Cardfood = (props: Props) => {
   return (
     <div className="card card-compact max-w-sm bg-base-100">
       <figure className='m-0'>
-        <img className="w-72 h-28 md:h-64 rounded-2xl" src={props.image} alt={props.heading} />
+        <Link to={`food_category/${props.heading}`} >
+          <img className="w-72 h-28 md:h-64 rounded-2xl" src={props.image} alt={props.heading} />
+        </Link>
       </figure>
-      <div className="card-body items-center">
-        <Link className="card-title m-0 font-bold no-underline" to={`food_category/${props.heading}`}>{props.heading}</Link>
+      <div className="card-body items-center text-center">
+        <Link className="card-title m-0 font-bold no-underline" to={`food_category/${props.heading}`}>{props.heading.toUpperCase()}</Link>
       </div>
     </div >
   )
